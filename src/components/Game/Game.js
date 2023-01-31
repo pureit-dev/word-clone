0c824d6186f166c38440664d3b68b223da8c0ba5
Keith Gibson <keithwgibson74@gmail.com>
2023-01-31 14:14:35 +0000

    Added a way of storing the results

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react"
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-import Form from '../Form/Form'
+import { sample } from "../../utils"
+import { WORDS } from "../../data"
+
+import GuessInput from "../GuessInput"
+import GuessResults from "../GuessResults"
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
-
-
-
+console.info({ answer })
 
 function Game() {
+	const [guesses, setGuesses] = React.useState([])
 
-  const [guess, setGuess] = React.useState("")
-  console.log(guess)
+	function handleSubmitGuess(tentativeGuess) {
+		setGuesses([...guesses, tentativeGuess])
+	}
 
-  return <>
-          <Form 
-            setGuess={setGuess}
-          />
-        </>;
+	return (
+		<>
+			<GuessResults guesses={guesses} />
+			<GuessInput handleSubmitGuess={handleSubmitGuess} />
+		</>
+	)
 }
 
-export default Game;
+export default Game
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+function GuessInput({ handleSubmitGuess }) {
+	const [tentativeGuess, setTentativeGuess] = React.useState("")
+
+	function handleSubmit(event) {
+		event.preventDefault()
+
+		if (tentativeGuess.length !== 5) {
+			window.alert("Please enter exactly 5 characters. 💖")
+			return
+		}
+
+		handleSubmitGuess(tentativeGuess)
+
+		setTentativeGuess("")
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className="guess-input-wrapper">
+			<label htmlFor="guess-input">Enter guess:</label>
+			<input
+				required
+				minLength={5}
+				maxLength={5}
+				value={tentativeGuess}
+				onChange={(event) => {
+					const nextGuess = event.target.value.toUpperCase()
+					setTentativeGuess(nextGuess)
+				}}
+				id="guess-input"
+				type="text"
+			/>
+		</form>
+	)
+}
+
+export default GuessInput
