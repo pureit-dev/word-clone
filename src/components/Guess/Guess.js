bd9d514130e76f909ecc3df8496d30b6d6dace53
Keith Gibson <keithgibson@Keiths-MacBook-Air.local>
2023-01-31 22:04:26 +0000

    Interim

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({index,value}) {
- 
+function Guess({index,value, answer}) {
+  
   return (
 		  <p className="guess">
-        {range(5).map(num => (
-
-          <span key={num} className="cell">{value ? value[num] : undefined}</span>
-        )
-          
-        )}
+        {range(5).map(num => {
+          checkGuess(value[num], answer)
+         return <span key={num} className="cell">{value ? value[num] : undefined}</span>
+        })}
 				
 				
 			</p>
